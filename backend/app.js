7c5f679c3006316102479c67ac7714468bfa9070
Pankaj Tyagi <itstyagi0001@gmail.com>
2024-12-21 19:53:41 +0530

    Created routes for user and transaction

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const userRoutes = require("./routes/user.routes");
+const transactionRoutes = require("./routes/transaction.routes");
 const app = express();
 const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/api/users", userRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 // Database Connection
 const { connectDB } = require("./config/database");
---
+++ backend/routes/transaction.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { createTransaction } = require("../controllers/transactionController");
+
+const router = express.Router();
+
+router.post("/", createTransaction);
+
+module.exports = router;
